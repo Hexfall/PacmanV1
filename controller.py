433bcea5b71a803fd72c17aecb579627c7a94009
Viktor Máni Mønster <viktor@mani.monster>
2025-03-13 22:38:28 +0100

    Estimates closer ghosts as more dangerous than further away ghosts

--- controller.py
+++ controller.py
@@ -6,11 +6,14 @@ from pellets import PelletGroup, Pellet
 from math import inf
 import numpy as np
 
+from vector import Vector2
+
 MAX_DEPTH = 14
 GREED_FACTOR = 25
 GHOST_VALUE = -20
 MAX_FEAR = 8 # Don't take ghost further than this many nodes into account
-FEAR_FACTOR = 2.5
+FEAR_FACTOR = 1.5
+BOARD_RADIUS = Vector2(NROWS * TILEHEIGHT, NCOLS * TILEWIDTH).magnitude()
 
 class Controller(Pacman):
     def __init__(self, node: Node) -> None:
@@ -96,7 +99,8 @@ class Controller(Pacman):
         ghosts = self.has_ghost(prev, cur)
         if not power and len(ghosts) != 0:
             dir_dot = -1
-            danger = False
+            dangerous = False
+            danger = 0
             for ghost in ghosts:
                 if ghost.mode.current == SPAWN:
                     continue
@@ -104,22 +108,26 @@ class Controller(Pacman):
                     if ghost.mode.timer / ghost.mode.time < .85:
                         s += GHOST_VALUE * GREED_FACTOR * self.ghost_multiplier
                         continue
-                danger = True
+                dangerous = True
                 ghost_dir = self.directions[ghost.direction]
                 ghost_dir = np.array([ghost_dir.x, ghost_dir.y])
                 to_node = prev.position - ghost.position
                 to_node = np.array([to_node.x, to_node.y])
                 to_node = to_node / np.linalg.norm(to_node)
                 dir_dot = max(np.dot(to_node, ghost_dir), dir_dot)
-            #if dir_dot > 0:
-            #    return 0
-            if danger:
                 if depth == 0 and dir_dot > .8:
                     return inf
+                danger = max(
+                    danger,
+                    ((MAX_FEAR - depth+4)/3)**5 * FEAR_FACTOR * max((dir_dot + 1)/2, 0) / (1 + (self.position - ghost.position).magnitude() / BOARD_RADIUS)
+                )
+            #if dir_dot > 0:
+            #    return 0
+            if dangerous:
                 if depth >= MAX_FEAR:
                     return 0
                 else:
-                    return ((MAX_FEAR - depth+4)/3)**5 * FEAR_FACTOR * max((dir_dot + 1)/2, 0)
+                    return danger
         if between:
             pellets = 0
         else:
@@ -132,7 +140,7 @@ class Controller(Pacman):
             if next is None or next in seen:
                 continue
             s += self.get_weight(next, cur, depth + 1, seen, power or p)
-        return s / cur.position.magnitude()
+        return s / (1 + cur.position.magnitude() / BOARD_RADIUS) / (depth + 1)
 
     def eatPellets(self, pellet_list: list[Pellet]) -> None | Pellet:
         for pellet in pellet_list:
